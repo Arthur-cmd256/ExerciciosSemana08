86ceda3fb10d6d4b7b9970c1b274bc2d310d732f
Arthur-cmd256 <arthur.nascimento2020@outlook.com>
2021-09-25 16:58:11 -0300

    Subindo resolução do exercicio 5

---
+++ Oviparo.java
@@ -0,0 +1,18 @@
+public class Oviparo extends Animal{
+    private String nome;
+    private int quantidadeOlhos;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public void setQuantidadeOlhos(int quantidadeOlhos) {
+        this.quantidadeOlhos = quantidadeOlhos;
+    }
+    public int getQuantidadeOlhos() {
+        return quantidadeOlhos;
+    }
+}
